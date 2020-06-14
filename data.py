25b424e4378be1316a4ad22636d79262c731baa0
Zhiwen Xie <zhiwenxie1900@outlook.com>
2020-06-14 14:33:29 +0800

    implemented MLP for Argoverse motion forecasting

---
+++ data.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+import pandas as pd
+import numpy as np
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+ROOT_DIR = "/ssd/datasets/argoverse/argoverse-forecasting-dataset/"
+TRAIN = ROOT_DIR + "train/data"
+VAL = ROOT_DIR + "val/data"
+TEST = ROOT_DIR + "test_obs/data/"
+DATASET_PATH = {
+        "train" : TRAIN,
+        "val" : VAL,
+        "test" : TEST
+        }
+
+class TrajectoryDataset(Dataset):
+    
+    def __init__(self, root_dir, mode):
+        self.root_dir = Path(root_dir)
+        self.mode = mode
+        self.sequences = [(self.root_dir / x).absolute() for x in os.listdir(self.root_dir)]
+        self.obs_len = 20
+        
+    def __len__(self):
+        return len(self.sequences)
+    
+    def __getitem__(self, idx):
+        sequence = pd.read_csv(self.sequences[idx])
+        agent_x = sequence[sequence["OBJECT_TYPE"] == "AGENT"]["X"]
+        agent_y = sequence[sequence["OBJECT_TYPE"] == "AGENT"]["Y"]
+        agent_traj = np.column_stack((agent_x, agent_y)).astype(np.float32)
+        # return {"input": agent_traj[:self.obs_len], "target": agent_traj[self.obs_len:]}
+        return [agent_traj[:self.obs_len], agent_traj[self.obs_len:]]
+
+def get_dataset(modes):
+    return (TrajectoryDataset(DATASET_PATH[mode], mode) for mode in modes)
